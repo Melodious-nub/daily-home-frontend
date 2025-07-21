131ea5446886922ab9ab8547e27c83000143e1e0
Shawon Talukder <shawon.talukder@green-delta.com>
2025-07-21 12:29:15 +0600

    u

--- src/app/pages/members/members.ts
+++ src/app/pages/members/members.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, DestroyRef, OnInit } from '@angular/core';
+import { Api } from '../../core/api';
 
 @Component({
   selector: 'app-members',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './members.html',
   styleUrl: './members.css'
 })
-export class Members {
+export class Members implements OnInit {
+  members: any = [];
+
+  constructor(private destroyRef: DestroyRef, private api: Api) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  fetchMember() {
+    
+  }
+
+  openAddMemberModal() {}
 
 }

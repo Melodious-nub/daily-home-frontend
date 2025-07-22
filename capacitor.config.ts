b9566407cd00f6bfacd1aca89feeda997f2b24d6
Melodious-nub <shawon.talukder1@gmail.com>
2025-07-22 23:18:59 +0600

    added with capacitor support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shawon.dailyhome',
+  appName: 'DailyHome',
+  webDir: 'dist/daily-home/browser'
+};
+
+export default config;

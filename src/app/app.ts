08c6e4f8df292e0549e1505bce23d7ccdc2c0678
Melodious-nub <shawon.talukder1@gmail.com>
2025-07-26 04:02:07 +0600

    updated with navbbar and statusbar fixed

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,7 @@ import { Header } from "./layout/header/header";
 import { BottomNav } from "./layout/bottom-nav/bottom-nav";
 import { Capacitor } from '@capacitor/core';
 import { StatusBar, Style } from '@capacitor/status-bar';
+import { NavigationBar } from '@capgo/capacitor-navigation-bar';
 
 @Component({
   selector: 'app-root',
@@ -19,6 +20,7 @@ export class App implements OnInit {
   ngOnInit(): void {
     if (Capacitor.isNativePlatform()) {
       this.configureStatusBar();
+      this.setNavigationBar();
     }
   }
 
@@ -30,6 +32,14 @@ export class App implements OnInit {
       await StatusBar.show();
     } catch (error) {
       console.error('Failed to configure status bar:', error);
+    }
+  }
+
+  async setNavigationBar() {
+    try {
+      await NavigationBar.setNavigationBarColor({ color: '#ffffff', darkButtons: true }); // Black nav bar
+    } catch (error) {
+      console.error('Navigation bar setup failed', error);
     }
   }
 }

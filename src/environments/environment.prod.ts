0c9f98a87f46cf54bcbff358d087f6e85366d2e2
Shawon Talukder <shawon.talukder@green-delta.com>
2025-08-14 16:50:32 +0600

    updated with env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://daily-home-backend.onrender.com/api/',
-  wsUrl: 'https://daily-home-backend.onrender.com',
+  apiUrl: 'https://daily-home-backend-dev.onrender.com/api/',
+  wsUrl: 'https://daily-home-backend-dev.onrender.com',
   appName: 'DailyHome'
 };
